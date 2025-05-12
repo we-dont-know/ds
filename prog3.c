e09b52c70edef2c8d1c5de4c583fc25f50f9e613
labs <mounikatjnvt@gmail.com>
2025-05-12 08:40:33 +0530

    prog3.c

---
+++ prog3.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,j,k,l;
+    char a[20],c[20],d[20];
+    
+    printf("eneter the message");
+    scanf("%s",a);
+    l=strlen(a);
+    for(i=0,j=0;i<l;i++)
+    {
+        if(i%2==0)
+        {
+            c[j++]=a[i];
+        }
+    }
+    for(i=0;i<l;i++)
+    {
+        if(i%2==1)
+        {
+            c[j++]=a[i];
+        }
+    }
+    c[j]='\0';
+    printf("encryptrd text: ");
+    printf("%s",c);
+    
+    if(l%2==0)
+k=l/2;
+else
+k=(l/2)+1;
+for(i=0,j=0;i<k;i++)
+{
+d[j]=c[i];
+j=j+2;
+}
+for(i=k,j=1;i<l;i++)
+{
+d[j]=c[i];
+j=j+2;
+}
+d[l]='\0';
+printf("\nText after decryption : ");
+printf("%s",d);
+}
